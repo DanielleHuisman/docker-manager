d56fcc6ec86db784c97903e7ddcc4ba54dcf0474
Daniel Huisman <daniel@huisman.me>
2020-03-17 13:04:53 +0100

    Add basic CLI parser

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log('Docker Manager CLI');
+import yargs from 'yargs';
+
+const services = ['test', 'test2'];
+
+const argv = yargs
+    .scriptName('docker-manager')
+    .choices('service', services)
+    .command('start <service>', 'Start a service', () => {
+        yargs
+            .positional('service', {
+                describe: 'Service'
+            });
+    })
+    .command('status <service>', 'Display status of a service', () => {
+        yargs
+            .positional('service', {
+                describe: 'Service'
+            });
+    })
+    .command('logs <service>', 'Display logs of a service', () => {
+        yargs
+            .positional('service', {
+                describe: 'Service'
+            })
+            .option('follow', {
+                alias: 'f',
+                describe: 'Follow log output'
+            });
+    })
+    .demandCommand()
+    .recommendCommands()
+    .strict()
+    .help()
+    .parse();
+
+console.log(argv);
