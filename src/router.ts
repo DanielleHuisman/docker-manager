3499d8e269e923d9962af7d002eded1d9b5b6ca4
Daniel Huisman <daniel@huisman.me>
2020-03-24 00:53:02 +0100

    Add basic applications table

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,26 @@
 import {Context} from '@danielhuisman/koa-base';
 import Router from 'koa-router';
 
+import {getApplicationNames, getServiceNames} from './docker';
+
 const router = new Router<any, Context>();
 
 router.get('/', async (ctx) => {
     return ctx.render('index');
+});
+
+router.get('/applications', async (ctx) => {
+    const applicationNames = await getApplicationNames();
+    const serviceNames = {};
+
+    for (const applicationName of applicationNames) {
+        serviceNames[applicationName] = await getServiceNames([applicationName]);
+    }
+
+    return ctx.render('applications', {
+        applicationNames,
+        serviceNames
+    });
 });
 
 export default router;
