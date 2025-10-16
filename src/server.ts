bf1cfbf6b0a64c040520a67452e9dd0249c1c952
Daniëlle Huisman <danielle@huisman.me>
2025-10-16 09:51:32 +0200

    Use URL param instead of header for token

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {type Request} from 'express';
+import express from 'express';
 import morgan from 'morgan';
 
 import {update} from './actions';
@@ -9,13 +9,8 @@ const app = express();
 app.disable('x-powered-by');
 app.use(morgan('combined'));
 
-const authorize = async (req: Request, applicationName: string) => {
-    const authorization = req.get('authorization');
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-        return false;
-    }
-
-    const token = await getTokenByValue(authorization.substring(7));
+const authorize = async (applicationName: string, tokenValue: string) => {
+    const token = await getTokenByValue(tokenValue);
     if (!token) {
         return false;
     }
@@ -27,8 +22,8 @@ const authorize = async (req: Request, applicationName: string) => {
     return true;
 };
 
-app.post('/applications/:applicationName', async (req, res) => {
-    const {applicationName} = req.params;
+app.post('/applications/:applicationName/:tokenValue', async (req, res) => {
+    const {applicationName, tokenValue} = req.params;
 
     if (!(await isApplication(applicationName))) {
         res.status(403);
@@ -36,7 +31,7 @@ app.post('/applications/:applicationName', async (req, res) => {
         return;
     }
 
-    if (!(await authorize(req, applicationName))) {
+    if (!(await authorize(applicationName, tokenValue))) {
         res.status(403);
         res.send(null);
         return;
@@ -53,8 +48,8 @@ app.post('/applications/:applicationName', async (req, res) => {
     res.send(null);
 });
 
-app.post('/applications/:applicationName/services/:serviceName', async (req, res) => {
-    const {applicationName, serviceName} = req.params;
+app.post('/applications/:applicationName/services/:serviceName/:tokenValue', async (req, res) => {
+    const {applicationName, serviceName, tokenValue} = req.params;
 
     if (!(await isApplication(applicationName)) || !(await isService(applicationName, serviceName))) {
         res.status(403);
@@ -62,7 +57,7 @@ app.post('/applications/:applicationName/services/:serviceName', async (req, res
         return;
     }
 
-    if (!(await authorize(req, applicationName))) {
+    if (!(await authorize(applicationName, tokenValue))) {
         res.status(403);
         res.send(null);
         return;
